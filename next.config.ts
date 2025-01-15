596051ff4ce595c5d87cde184a1e258239a42e68
kat-kel <klarinet28@gmail.com>
2025-01-15 17:32:45 +0100

    deploy under new name

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const debug = process.env.NODE_ENV !== 'production';
+
 const nextConfig: NextConfig = {
+    assetPrefix: !debug ? '/' : '',
+    basePath: '/',
     output: 'export',
     distDir: 'dist',
     images: {
