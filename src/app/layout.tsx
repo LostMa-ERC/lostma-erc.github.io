a97e4defa35a54238aa549a77b18d8f92fcb452d
kat-kel <klarinet28@gmail.com>
2025-01-21 13:16:24 +0100

    test og meta tags 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,23 @@ export default function RootLayout({
     <html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>LostMa</title>
     <link rel="icon" type="image/svg+xml" href="./favicon.svg" />
     <link rel="shortcut icon" href="./favicon.ico" />
+    <title>LostMa</title>
+    <meta name="description" content="ERC Project"/>
+    {/* Open Graph meta tags */}
+    <meta property="og:url" content="https://lostma-erc.github.io/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="LostMa"/>
+    <meta property="og:description" content="ERC Project"/>
+    <meta property="og:image" content="public/images/lostma_logo.png"/>
+    {/* Twitter meta tags */}
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta property="twitter:domain" content="lostma-erc.github.io"/>
+    <meta property="twitter:url" content="https://lostma-erc.github.io/"/>
+    <meta name="twitter:title" content="LostMa"/>
+    <meta name="twitter:description" content="ERC Project"/>
+    <meta name="twitter:image" content="public/images/lostma_logo.png"/>
   </head>
   <body className="
     prefers-color-scheme 
