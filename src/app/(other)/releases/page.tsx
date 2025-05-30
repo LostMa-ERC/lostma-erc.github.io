29daa8fce3ca756067bfe5c4f6b53805168c8090
kat-kel <klarinet28@gmail.com>
2025-05-30 12:00:22 +0200

    scrapers

--- src/app/(other)/releases/page.tsx
+++ src/app/(other)/releases/page.tsx
@@ -15,7 +15,7 @@ export default function News() {
         </h2>
 
         {/* First dataset release */}
-        <div className="bg-white/50 rounded-md md:rounded-lg">
+        <div className="rounded-md md:rounded-lg py-2">
           <div className="bg-light/30 text-white rounded-md md:rounded-lg">
             <div className="grid md:grid-cols-2 p-4">
               <div className="pt-4 md:pt-2 text-black">
@@ -42,7 +42,7 @@ export default function News() {
         </h2>
 
         {/* Heurist CLI */}
-        <div className="bg-white/50 rounded-md md:rounded-lg">
+        <div className="rounded-md md:rounded-lg py-2">
           <div className="bg-light/30 text-white rounded-md md:rounded-lg">
             <div className="grid md:grid-cols-2 p-4">
               <div className="pt-4 md:pt-2 text-black">
@@ -66,6 +66,57 @@ export default function News() {
           </div>
         </div>
 
+        {/* French scrapers */}
+        <div className="rounded-md md:rounded-lg py-2">
+          <div className="bg-light/30 pt-4 md:pt-2 text-black rounded-md md:rounded-lg">
+            <h3 className="text-lg md:text-xl p-4">Scrapers for Cultural Heritage sites</h3>
+            <div className="grid md:grid-cols-2 p-4">
+              <div>
+                  <a href="https://github.com/LostMa-ERC/JonasScraper" target="_blank" rel="noreferrer">
+                    <h2 className="md:text-2xl text-xl italic">Jonas</h2>
+                  </a>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    Scrape metadata about manuscripts and works on the <a className="link" href="https://jonas.irht.cnrs.fr/" target="_blank" rel="noreferrer">website Jonas</a> and its <em>Répertoire des textes et livres français et occitans (850-1550)</em> database, which is managed by the <a className="link" href="https://www.irht.cnrs.fr" target="_blank" rel="noreferrer">Institut de Recherche et d&apos;Histoire des Textes (IRHT)</a>.
+                  </p>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    Provide the scraper with the URL of a manuscript (jonas.irht.cnrs.fr/manuscrit/) or work (jonas.irht.cnrs.fr/oeuvre/) and receive relational tables of the manuscrit or work, depending on the URL, and the witnesses related to it.
+                  </p>
+                  <a className="grid place-content-center" href="https://github.com/LostMa-ERC/JonasScraper" target="_blank" rel="noreferrer">
+                    <p className="link font-light text-xs md:text-base tracking-wide md:px-8 w-fit">https://github.com/LostMa-ERC/JonasScraper</p>
+                  </a>
+              </div>
+              <div>
+                  <a href="https://github.com/LostMa-ERC/french-catalogue-scraper" target="_blank" rel="noreferrer">
+                    <h2 className="md:text-2xl text-xl italic">Catalogue collectif de France, Archives et Manuscrits</h2>
+                  </a>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    Scrape bibliographic metadata from notices in the <a className="link" href="https://ccfr.bnf.fr/" target="_blank" rel="noreferrer">Catalogue collectif de France (CCfr)</a> and/or the Bibliothèque nationale de France&apos;s <a className="link" href="https://archivesetmanuscrits.bnf.fr/" target="_blank" rel="noreferrer">Archives et Manuscrits</a> catalogue.
+                  </p>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    Both scrapers are installed with the same Python package and require the URL of the notice to be scraped.
+                  </p>
+                  <a className="grid place-content-center" href="https://github.com/LostMa-ERC/french-catalogue-scraper" target="_blank" rel="noreferrer">
+                    <p className="link font-light text-xs md:text-base tracking-wide md:px-8 w-fit">https://github.com/LostMa-ERC/french-catalogue-scraper</p>
+                  </a>
+              </div>
+              <div>
+                  <a href="https://github.com/LostMa-ERC/search-archives-manuscrits" target="_blank" rel="noreferrer">
+                    <h2 className="md:text-2xl text-xl italic">Archives et Manuscrits (search)</h2>
+                  </a>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    A CLI that runs the advanced search feature of the Bibliothèque nationale de France&apos;s <a className="link" href="https://archivesetmanuscrits.bnf.fr/" target="_blank" rel="noreferrer">Archives et Manuscrits</a> website.
+                  </p>
+                  <p className="font-light text-xs md:text-base tracking-wide md:px-8">
+                    Using the department (i.e. Arsenal) and the shelfmark (<em>cote</em>), find the notice for the document in the Archives et Manuscrits catalogue. This tool is particularly useful when combined with the Archives et Manuscrits scraper, which takes the discovered notice URL.
+                  </p>
+                  <a className="grid place-content-center" href="https://github.com/LostMa-ERC/search-archives-manuscrits" target="_blank" rel="noreferrer">
+                    <p className="link font-light text-xs md:text-base tracking-wide md:px-8 w-fit">https://github.com/LostMa-ERC/search-archives-manuscrits</p>
+                  </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
       <div className="section-block py-2">
@@ -74,7 +125,7 @@ export default function News() {
         </h2>
 
         {/* First model release */}
-        <div className="bg-white/50 rounded-md md:rounded-lg">
+        <div className="rounded-md md:rounded-lg py-2">
           <div className="bg-light/30 text-white rounded-md md:rounded-lg">
             <div className="grid md:grid-cols-2 p-4">
               <div className="pt-4 md:pt-2 text-black">
